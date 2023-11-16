47c521b67706cd3deb1903496275e4240bdbe342
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2023-11-16 13:42:46 -0600

    generatePassword()

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,17 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+//This function inside of the writePassword() function, meaning that when writePassword is called, generatePassword will be called after.
+function generatePassword() {
+
+  //This is to check to see if the addEventListener is working properly
+  console.log("Button Clicked")
+
+
+  //This should return the result of the function to the password variable in the writePassword function
+  return "Generated Password";
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
