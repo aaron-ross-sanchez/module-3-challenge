daf53f391788c5642cd23665e75f1077456d38c6
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2023-11-16 13:47:24 -0600

    user inputs

--- Develop/script.js
+++ Develop/script.js
@@ -10,7 +10,18 @@ function generatePassword() {
   //This is to check to see if the addEventListener is working properly
   console.log("Button Clicked")
 
+  //Initial alert for instructions
+  alert("To generate a secure password, please press close and answer a few questions.");
 
+  //characterInput
+  let characterInput = prompt("Please enter the amount of characters between 8-128 that you would like to include in your password.");
+
+  //inputs for types of characters
+  let lowercaseInput = confirm("Would you like to include lowercase letters? Press OK for YES, CANCEL for NO.");
+  let uppercaseInput = confirm("Would you like to include uppercase letters? Press OK for YES, CANCEL for NO.");
+  let numbersInput = confirm("Would you like to include numbers? Press OK for YES, CANCEL for NO.");
+  let specialInput = confirm("Would you like to include special characters? Press OK for YES, CANCEL for NO.");
+  
   //This should return the result of the function to the password variable in the writePassword function
   return "Generated Password";
 }
