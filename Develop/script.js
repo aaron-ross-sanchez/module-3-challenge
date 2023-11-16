057399154ef42858f93e413e96e90c4a82992a22
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2023-11-16 13:54:46 -0600

    characterNumber

--- Develop/script.js
+++ Develop/script.js
@@ -15,6 +15,16 @@ function generatePassword() {
 
   //characterInput
   let characterInput = prompt("Please enter the amount of characters between 8-128 that you would like to include in your password.");
+  //parseFloat turns a string into a floating-point number.
+  let characterNumber = parseFloat(characterInput);
+  
+  //This will test if the number they entered is within the parameters wanted. If the number gets logged to the console, then it fits the parameters defined in the prompt()
+    if (!isNaN(characterNumber) && characterNumber > 7 && characterNumber < 129) {
+      console.log("The user entered a valid number:", characterNumber);
+    } else { 
+      //This will put an error in the console. This showed me that the following if statement works
+      throw new Error("Invalid input. Please enter a number between 8-128.");
+    }
 
   //inputs for types of characters
   let lowercaseInput = confirm("Would you like to include lowercase letters? Press OK for YES, CANCEL for NO.");
